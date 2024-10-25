fe7d6ba6f67a41e285cfae9d9a3bc1d8bc7dc5c0
awafityireal <av.guallakil.oficial.singluten797@gmail.com>
2024-10-25 18:44:22 -0400

    Optimizacion de sugerencia de sucursales

--- src/main/java/edd/bdi/proj/FuncionesBusqueda.java
+++ src/main/java/edd/bdi/proj/FuncionesBusqueda.java
@@ -14,8 +14,6 @@ import org.graphstream.graph.Node;
  * @author danielairibarren
  */
 public class FuncionesBusqueda {
-
-
     public void recorrerProfundidad(Grafo g, int v, boolean[] visitados, int[] distancia, int t, boolean test) {
         visitados[v] = true;
         for (int i = 0; i < g.getNumVertices(); i++) {
@@ -265,7 +263,6 @@ public class FuncionesBusqueda {
             Set cobertura = obtenerCobertura(g, parada, t);
             coberturas[i] = cobertura.size();
         }
-
         int maxCobertura = getMaxCobertura(coberturas);
         while (cubiertas.size() < g.getNumVertices()) {
             for (int i = 0; i < g.getNumVertices(); i++) {
@@ -274,9 +271,16 @@ public class FuncionesBusqueda {
                     sugeridas.add(parada);
                     cubiertas.addAll(obtenerCobertura(g, parada, t));
                     cubiertas.add(parada);
+                    coberturas[i] = -1;
+                    maxCobertura = getMaxCobertura(coberturas);
+
                 }
+                if (coberturas.length == (i+1)) {
+                    maxCobertura -=1;
+                }
+
             }
-            maxCobertura--;
+
         }
 
         return sugeridas;
@@ -325,5 +329,4 @@ public class FuncionesBusqueda {
 
         return cobertura;
     }
-
 }
